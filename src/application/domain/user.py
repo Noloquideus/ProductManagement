603dd928666cb708d930241d4f38798351109206
Noloquideus <daniilmanukian@gmail.com>
2024-06-25 14:47:08 +0300

    feat: add model for registration

---
+++ src/application/domain/user.py
@@ -0,0 +1,42 @@
+import phonenumbers
+from pydantic import BaseModel, StrictStr, EmailStr, Field, field_validator
+from src.exceptions import InvalidPhoneNumberException, InvalidPhoneNumberFormatException
+
+
+class UserRegistration(BaseModel):
+    phone_number: StrictStr = Field(
+        title="Phone number",
+        description="Phone number of the user",
+        example="+79111111111")
+
+    email: EmailStr = Field(
+        title="Email",
+        description="Email of the user",
+        example="example@example.com")
+
+    first_name: StrictStr = Field(
+        title="First name",
+        description="First name of the user",
+        example="John")
+
+    last_name: StrictStr = Field(
+        title="Last name",
+        description="Last name of the user",
+        example="Doe")
+
+    password: StrictStr = Field(
+        title="Password",
+        description="Password of the user",
+        example="password",
+        min_length=8,
+        max_length=128)
+
+    @field_validator('phone_number')
+    def validate_phone_number(cls, phone_number):
+        try:
+            parsed_number = phonenumbers.parse(phone_number)
+            if not phonenumbers.is_valid_number(parsed_number):
+                raise InvalidPhoneNumberException
+        except Exception as e:
+            raise InvalidPhoneNumberFormatException from e
+        return phone_number
