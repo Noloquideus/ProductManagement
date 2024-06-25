e006aa1bf32c955cab8033aa3f8ddb9ad3b80742
Noloquideus <daniilmanukian@gmail.com>
2024-06-25 14:39:30 +0300

    feat: add config

---
+++ src/config.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv, find_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv(find_dotenv(".env"))
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    ALGORITHM: str
+    ACCESS_SECRET_KEY: str
+    ACCESS_TTL_MINUTES: int
+    REFRESH_SECRET_KEY: str
+    REFRESH_TTL_MINUTES: int
+
+    SUPERADMIN_EMAIL: str
+    SUPERADMIN_PHONE: str
+    SUPERADMIN_FIRSTNAME: str
+    SUPERADMIN_LASTNAME: str
+    SUPERADMIN_PASSWORD: str
+    SUPERADMIN_ACCESS_LEVEL: int
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()
