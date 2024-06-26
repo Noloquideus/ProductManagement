1003166650bf1e972ab6d3aabb0c123606b4df20
Noloquideus <daniilmanukian@gmail.com>
2024-06-26 21:18:08 +0300

    fix: all update

--- src/api/auth/decorator.py
+++ src/api/auth/decorator.py
@@ -1,26 +1,39 @@
-from functools import wraps
 from typing import Callable
-from src.exceptions import InvalidTokenException, TokenExpiredException
+from functools import wraps
+from src.exceptions import InvalidTokenException, InsufficientAccessLevelException
 from src.infrastructure.utils.token_service import TokenService
 
 
 def require_min_access_level(min_access_level: int):
-    def decorator(func: Callable) -> Callable:
+    """
+       Minimal level decorator providing access for routes and token validation
+
+       Args:
+           min_access_level (int): The minimum access level required to access the route.
+
+       Returns:
+           Callable: The decorated function with access level enforcement.
+
+       Example:
+           @app.get("/ping")
+            @require_min_access_level(min_access_level=1)
+            async def ping(access_token: str = Cookie('access_token')):
+                return {"message": "pong"}
+       """
+    def decorator(func: Callable):
         @wraps(func)
-        async def wrapper(*args, **kwargs):
-            request = args[0]
-            access_token = request.cookies.get("access_token")
-            if not access_token:
-                raise InvalidTokenException
+        async def decorated(*args, access_token, **kwargs):
             try:
                 payload = await TokenService.decode_access_token(access_token)
-                user_access_level = payload.get("access_level")
-                if int(user_access_level) < min_access_level:
-                    raise InvalidTokenException
-            except (InvalidTokenException, TokenExpiredException) as e:
-                raise e
+            except Exception as e:
+                raise InvalidTokenException
+
+            access_level = payload.get("access_level", 0)
+            print(access_level)
+
+            if access_level < min_access_level:
+                raise InsufficientAccessLevelException
+
             return await func(*args, **kwargs)
-
-        return wrapper
-
+        return decorated
     return decorator
