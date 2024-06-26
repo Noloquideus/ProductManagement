ed56dc6e174c1b1d37e5823fececa86dce87eb17
Noloquideus <daniilmanukian@gmail.com>
2024-06-26 18:25:30 +0300

    feat: add create_user method

--- src/infrastructure/database/repositories/user_repository.py
+++ src/infrastructure/database/repositories/user_repository.py
@@ -1,5 +1,10 @@
 from uuid import UUID
+
+from sqlalchemy.exc import IntegrityError
+
 from src.application.domain.user import UserDto
+from src.exceptions import UserAlreadyExistsException
+from src.infrastructure.database.database import async_session_maker
 from src.infrastructure.database.models import User
 
 
@@ -7,7 +12,21 @@ class UserRepository:
 
     @staticmethod
     async def create_user(user_data: UserDto) -> User:
-        pass
+        async with async_session_maker() as session:
+            try:
+                user = User(
+                    email=user_data.email,
+                    phone_number=user_data.phone_number,
+                    first_name=user_data.first_name,
+                    last_name=user_data.last_name,
+                    password_hash=user_data.password_hash)
+                session.add(user)
+                await session.commit()
+                return user
+
+            except IntegrityError:
+                await session.rollback()
+                raise UserAlreadyExistsException
 
     @staticmethod
     async def add_refresh_token(user_id: UUID, refresh_token: str) -> None:
