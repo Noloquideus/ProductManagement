bc1664c1fd3d054833e8813f16b8d7a022342e4d
Noloquideus <daniilmanukian@gmail.com>
2024-06-26 18:25:27 +0300

    feat: add create_user method

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -1,12 +1,24 @@
-from src.application.domain.user import UserDto
+from src.application.domain.user import UserDto, UserRegistration
 from src.infrastructure.database.models import User
+from src.infrastructure.database.repositories.user_repository import UserRepository
+from src.infrastructure.utils.hash_service import HashService
 
 
 class UserService:
 
     @staticmethod
-    async def create_user(user_data: UserDto) -> User:
-        pass
+    async def create_user(user_data: UserRegistration) -> User:
+
+        user_dto = UserDto(
+            phone_number=user_data.phone_number,
+            email=user_data.email,
+            first_name=user_data.first_name,
+            last_name=user_data.last_name,
+            password_hash=HashService.hash(user_data.password))
+
+        user = await UserRepository.create_user(user_dto)
+
+        return user
 
     @staticmethod
     async def get_user_by_id(user_id: int) -> User:
