db48b92e0a9f1938b7295e14a2332f910678c843
Noloquideus <daniilmanukian@gmail.com>
2024-06-26 18:14:56 +0300

    feat: all async

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -5,18 +5,18 @@ from src.infrastructure.database.models import User
 class UserService:
 
     @staticmethod
-    def create_user(user_data: UserDto) -> User:
+    async def create_user(user_data: UserDto) -> User:
         pass
 
     @staticmethod
-    def get_user_by_id(user_id: int) -> User:
+    async def get_user_by_id(user_id: int) -> User:
         pass
 
     @staticmethod
-    def get_user_by_email(email: str) -> User:
+    async def get_user_by_email(email: str) -> User:
         pass
 
     @staticmethod
-    def get_user_by_phone(phone: str) -> User:
+    async def get_user_by_phone(phone: str) -> User:
         pass
 
