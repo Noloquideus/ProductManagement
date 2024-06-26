289b0ef919cebe0773996068a33e0b0084f67fb5
Noloquideus <daniilmanukian@gmail.com>
2024-06-26 18:14:09 +0300

    feat: create token service and create_access_token method

---
+++ src/infrastructure/utils/token_service.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta, timezone
+import jwt
+from src.application.domain.user import UserDto
+from src.config import settings
+
+
+class TokenService:
+
+    @staticmethod
+    async def create_access_token(user: UserDto) -> str:
+        expire_delta = timedelta(minutes=settings.ACCESS_TTL_MINUTES)
+        payload = {
+            "sub": user.id,
+            "email": user.email,
+            "phone": user.phone_number,
+            "access_level": user.access_level,
+            "is_email_verified": user.is_email_verified,
+            "iat": datetime.now(timezone.utc),
+            "exp": datetime.now(timezone.utc) + expire_delta
+        }
+        token = jwt.encode(payload, settings.ACCESS_SECRET_KEY, algorithm=settings.ALGORITHM)
+        return token
