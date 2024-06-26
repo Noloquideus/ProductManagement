7a431cbf5c85f90ca9aa014ec81d1c47d3a32e5c
Noloquideus <daniilmanukian@gmail.com>
2024-06-26 20:12:51 +0300

    feat: add InvalidTokenException and TokenExpiredException

--- src/exceptions.py
+++ src/exceptions.py
@@ -32,3 +32,13 @@ class UserAlreadyExistsException(DefaultException):
 class InvalidPasswordException(DefaultException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid password')
+
+
+class TokenExpiredException(DefaultException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token expired')
+
+
+class InvalidTokenException(DefaultException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
