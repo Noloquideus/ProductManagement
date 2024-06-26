fcdbe409b11d4ac57e646825bee3bd303343922c
Noloquideus <daniilmanukian@gmail.com>
2024-06-26 18:48:41 +0300

    feat: add InvalidPasswordException

--- src/exceptions.py
+++ src/exceptions.py
@@ -28,3 +28,7 @@ class UserAlreadyExistsException(DefaultException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_409_CONFLICT, detail='User already exists')
 
+
+class InvalidPasswordException(DefaultException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid password')
