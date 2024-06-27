950723f2a6a2b76313d8cc5a179b69649a7486af
Noloquideus <daniilmanukian@gmail.com>
2024-06-27 21:59:02 +0300

    feat: create del product method

--- src/application/services/product_service.py
+++ src/application/services/product_service.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from src.application.domain.product import ProductCreate
 from src.config import settings
 from src.infrastructure.database.repositories.product_repository import ProductRepository
@@ -8,3 +10,7 @@ class ProductService:
     @staticmethod
     async def create_product(product_data: ProductCreate):
         return await ProductRepository.create_product(product_data)
+
+    @staticmethod
+    async def delete_product(product_id: UUID):
+        return await ProductRepository.delete_product(product_id)
