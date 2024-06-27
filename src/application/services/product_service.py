bf63791eb96611c56a7370677359df4941af8275
Noloquideus <daniilmanukian@gmail.com>
2024-06-28 01:58:30 +0300

    feat: create get_products method with filter

--- src/application/services/product_service.py
+++ src/application/services/product_service.py
@@ -1,5 +1,6 @@
+from typing import Optional, List
 from uuid import UUID
-from src.application.domain.product import ProductCreate, ProductUpdate
+from src.application.domain.product import ProductCreate, ProductUpdate, ProductResponse
 from src.infrastructure.database.repositories.product_repository import ProductRepository
 
 
@@ -16,3 +17,18 @@ class ProductService:
     @staticmethod
     async def update_product(product_data: ProductUpdate):
         return await ProductRepository.update_product(product_data)
+
+    @staticmethod
+    async def get_product_by_id(product_id: UUID) -> Optional[ProductResponse]:
+        return await ProductRepository.get_product_by_id(product_id)
+
+    @staticmethod
+    async def get_products(
+        name: Optional[str],
+        min_price: Optional[float],
+        max_price: Optional[float],
+        min_quantity: Optional[int],
+        max_quantity: Optional[int],
+        category_id: Optional[UUID]
+    ) -> List[ProductResponse]:
+        return await ProductRepository.get_products(name, min_price, max_price, min_quantity, max_quantity, category_id)
