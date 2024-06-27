7dfb5fb5375b601957d89963f0003d01e66a981c
Noloquideus <daniilmanukian@gmail.com>
2024-06-27 04:03:09 +0300

    feat: add category and product model

--- src/infrastructure/database/models.py
+++ src/infrastructure/database/models.py
@@ -1,6 +1,8 @@
 import uuid
-from sqlalchemy import Column, UUID, String, DateTime, func
+from sqlalchemy import Column, UUID, String, DateTime, func, Float, ForeignKey
 from sqlalchemy.dialects.postgresql import ARRAY, INTEGER
+from sqlalchemy.orm import relationship
+
 from src.infrastructure.database.database import Base
 
 
@@ -15,3 +17,24 @@ class User(Base):
     password_hash = Column(String, nullable=False)
     access_level = Column(INTEGER, nullable=False, default=1)
     date_created = Column(DateTime, nullable=False, default=func.now())
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False, index=True)
+    name = Column(String, nullable=False, unique=True, index=True)
+    description = Column(String, nullable=True)
+    products = relationship("Product", back_populates="category")
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False, index=True)
+    name = Column(String, nullable=False, index=True)
+    description = Column(String, nullable=True)
+    price = Column(Float, nullable=False)
+    quantity = Column(INTEGER, nullable=False)
+    category_id = Column(UUID(as_uuid=True), ForeignKey('categories.id'), nullable=False)
+    category = relationship("Category", back_populates="products")
