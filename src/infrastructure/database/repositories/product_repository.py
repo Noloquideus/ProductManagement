1a7a6af9295838917d5de458837181167ec21650
Noloquideus <daniilmanukian@gmail.com>
2024-06-27 22:24:44 +0300

    feat: add update product method

--- src/infrastructure/database/repositories/product_repository.py
+++ src/infrastructure/database/repositories/product_repository.py
@@ -2,10 +2,10 @@ from uuid import UUID
 
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
-from src.application.domain.product import ProductCreate
-from src.exceptions import ProductAlreadyExistsException, ProductNotFoundException
+from src.application.domain.product import ProductCreate, ProductUpdate
+from src.exceptions import ProductAlreadyExistsException, ProductNotFoundException, CategoryNotFoundException
 from src.infrastructure.database.database import async_session_maker
-from src.infrastructure.database.models import Product
+from src.infrastructure.database.models import Product, Category
 
 
 class ProductRepository:
@@ -42,4 +42,38 @@ class ProductRepository:
                 raise ProductNotFoundException
             await session.delete(product)
             await session.commit()
+            return product
+
+    @staticmethod
+    async def update_product(product_data: ProductUpdate):
+        async with async_session_maker() as session:
+
+            result = await session.execute(select(Product).filter_by(id=product_data.product_id))
+            product = result.scalars().first()
+
+            if not product:
+                raise ProductNotFoundException
+
+            if product_data.name is not None:
+                product.name = product_data.name
+            if product_data.description is not None:
+                product.description = product_data.description
+            if product_data.price is not None:
+                product.price = product_data.price
+            if product_data.quantity is not None:
+                product.quantity = product_data.quantity
+            if product_data.category_id is not None:
+                result = await session.execute(select(Category).filter_by(id=product_data.category_id))
+                category = result.scalars().first()
+                if not category:
+                    raise CategoryNotFoundException
+
+                product.category_id = category.id
+
+            try:
+                await session.commit()
+            except SQLAlchemyError as e:
+                await session.rollback()
+                raise e
+
             return product
