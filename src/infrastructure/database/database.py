1b0a6b142e4b979a903d63290801b776ab3ab8d8
Noloquideus <daniilmanukian@gmail.com>
2024-06-27 03:27:08 +0300

    feat: add settings for tests

--- src/infrastructure/database/database.py
+++ src/infrastructure/database/database.py
@@ -1,8 +1,17 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from src.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL)
+
+if settings.MODE == 'TEST':
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    PARAMS = {'poolclass': NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    PARAMS = {}
+
+engine = create_async_engine(DATABASE_URL, **PARAMS)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
