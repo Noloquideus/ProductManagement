84aa660f59a50dd47f6069ca8db7e334f17b6db7
Noloquideus <daniilmanukian@gmail.com>
2024-06-27 20:13:36 +0300

    fix:

--- src/infrastructure/database/init/none_category.py
+++ src/infrastructure/database/init/none_category.py
@@ -6,9 +6,9 @@ from src.infrastructure.database.models import Category
 
 async def create_none_category():
     async with async_session_maker() as session:
-        existing_category = await session.execute(select(Category).filter_by(name=f'{settings.NONE_CATEGORY_NAME}'))
+        existing_category = await session.execute(select(Category).filter_by(name=settings.NONE_CATEGORY_NAME))
         category = existing_category.scalars().first()
         if not category:
-            new_category = Category(f'{settings.NONE_CATEGORY_NAME}', description='product without category')
+            new_category = Category(name=settings.NONE_CATEGORY_NAME, description='product without category')
             session.add(new_category)
-            await session.comm
+            await session.commit()
