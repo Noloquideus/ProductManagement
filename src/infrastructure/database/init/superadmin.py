5b5c3b07f551767364228ecfc78fed78d44563bc
Noloquideus <daniilmanukian@gmail.com>
2024-06-27 04:46:02 +0300

    fix: update

--- src/infrastructure/database/init/superadmin.py
+++ src/infrastructure/database/init/superadmin.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select, or_
+
 from src.config import settings
 from src.infrastructure.database.database import async_session_maker
 from src.infrastructure.database.models import User
@@ -6,13 +8,27 @@ from src.infrastructure.utils.hash_service import HashService
 
 async def create_superadmin():
     async with async_session_maker() as session:
+        existing_user = await session.execute(
+            select(User).where(
+                or_(
+                    User.email == settings.SUPERADMIN_EMAIL,
+                    User.phone_number == settings.SUPERADMIN_PHONE
+                )
+            )
+        )
+        user = existing_user.scalars().first()
+        if user is not None:
+            return
+
         superadmin = User(
             email=settings.SUPERADMIN_EMAIL,
             phone_number=settings.SUPERADMIN_PHONE,
             first_name=settings.SUPERADMIN_FIRSTNAME,
             last_name=settings.SUPERADMIN_LASTNAME,
             password_hash=HashService.hash(settings.SUPERADMIN_PASSWORD),
-            access_level=settings.SUPERADMIN_ACCESS_LEVEL)
+            access_level=settings.SUPERADMIN_ACCESS_LEVEL
+        )
         session.add(superadmin)
         await session.commit()
+
 
