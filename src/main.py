e6fbcfd40b2b7cf3de84ad87ffb336ef8bcbb854
Noloquideus <daniilmanukian@gmail.com>
2024-06-28 03:57:14 +0300

    feat: include admin views

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqladmin import Admin
+
+from src.api.admin.views import UserAdmin, CategoryAdmin, ProductAdmin
 from src.api.auth.auth_router import auth_router
 from src.api.category_router import category_router
 from src.api.product_router import product_router
+from src.infrastructure.database.database import engine
 from src.infrastructure.database.init.none_category import create_none_category
 from src.infrastructure.database.init.redis import init_redis
 from src.infrastructure.database.init.superadmin import create_superadmin
@@ -23,6 +27,11 @@ app = FastAPI(title='Auth Service', description='Auth Service API', version='1.0
 app.include_router(auth_router)
 app.include_router(category_router)
 app.include_router(product_router)
+
+admin = Admin(app, engine)
+admin.add_view(UserAdmin)
+admin.add_view(CategoryAdmin)
+admin.add_view(ProductAdmin)
 
 
 @app.get("/")
